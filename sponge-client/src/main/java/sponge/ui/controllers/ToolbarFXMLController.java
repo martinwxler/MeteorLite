dc8d64f428e3e56931f3f2cccd365bb2605ec1cc
therealnull <therealnull@gmail.com>
2021-06-22 03:21:49 -0400

    fxml: update toolbar

--- sponge-client/src/main/java/sponge/ui/controllers/ToolbarFXMLController.java
+++ sponge-client/src/main/java/sponge/ui/controllers/ToolbarFXMLController.java
@@ -4,17 +4,23 @@ import com.jfoenix.controls.JFXButton;
 import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
 import javafx.scene.text.Text;
 import osrs.Launcher;
 
 import java.io.IOException;
 
 public class ToolbarFXMLController {
-    @FXML private JFXButton settingsButton;
+    @FXML private JFXButton pluginsButton;
+    @FXML private JFXButton optionsButton;
     @FXML private Text title;
     
-    @FXML protected void handleSettingsPressed(ActionEvent event) {
+    @FXML protected void handlePluginsPressed(ActionEvent event) {
         Launcher.togglePluginsPanel();
+    }
+
+    @FXML protected void handleOptionsPressed(ActionEvent event) {
+
     }
 
     @FXML
