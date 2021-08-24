fff4d5f78c010a934e2f3d41928a6ab01aaea3f5
illumineawake <illumineawake@gmail.com>
2021-08-24 23:23:09 +1000

    walkerplugin: World Map Walking

--- meteor-client/src/main/java/meteor/plugins/api/example/walking/WalkerPlugin.java
+++ meteor-client/src/main/java/meteor/plugins/api/example/walking/WalkerPlugin.java
@@ -5,20 +5,25 @@ import meteor.config.ConfigManager;
 import meteor.eventbus.Subscribe;
 import meteor.plugins.Plugin;
 import meteor.plugins.PluginDescriptor;
+import meteor.plugins.api.entities.Players;
 import meteor.plugins.api.movement.Movement;
+import meteor.plugins.api.widgets.Widgets;
 import meteor.ui.overlay.OverlayLayer;
 import meteor.ui.overlay.OverlayManager;
 import meteor.ui.overlay.OverlayPosition;
 import meteor.ui.overlay.OverlayPriority;
-import net.runelite.api.GameState;
-import net.runelite.api.MenuAction;
+import meteor.ui.overlay.worldmap.WorldMapOverlay;
+import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.api.events.*;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
 @PluginDescriptor(
@@ -38,12 +43,19 @@ public class WalkerPlugin extends Plugin {
     private WalkerOverlay overlay;
 
     @Inject
+    private WorldMapOverlay worldMapOverlay;
+
+    @Inject
     private WalkerConfig config;
 
     @Provides
     public WalkerConfig getConfig(ConfigManager configManager) {
         return configManager.getConfig(WalkerConfig.class);
     }
+
+    private Point lastMenuOpenedPoint;
+    private boolean mapWalking;
+    private WorldPoint mapPoint;
 
     @Override
     public void startup() {
@@ -65,8 +77,8 @@ public class WalkerPlugin extends Plugin {
             return;
         }
 
-        if (config.walk()) {
-            WorldPoint walkPoint = new WorldPoint(config.x(), config.y(), client.getPlane());
+        if (config.walk() || mapWalking) {
+            WorldPoint walkPoint = mapWalking ? mapPoint : new WorldPoint(config.x(), config.y(), client.getPlane());
             logger.debug("Destination is {} {}", walkPoint.getX(), walkPoint.getY());
             overlay.setTile(walkPoint);
             executorService.execute(() -> Movement.walkTo(walkPoint));
@@ -74,9 +86,47 @@ public class WalkerPlugin extends Plugin {
     }
 
     @Subscribe
+    public void onMenuOpened(MenuOpened event) {
+        logger.info("Last menu opened point: {}", client.getMouseCanvasPosition());
+        lastMenuOpenedPoint = client.getMouseCanvasPosition();
+    }
+
+    @Subscribe
+    public void onMenuEntryAdded(MenuEntryAdded event) {
+        final Widget map = client.getWidget(WidgetInfo.WORLD_MAP_VIEW);
+
+        if (map == null) {
+            return;
+        }
+
+        if (map.getBounds().contains(client.getMouseCanvasPosition().getX(), client.getMouseCanvasPosition().getY())) {
+            addMenuEntry(event, "Map walk here");
+            addMenuEntry(event, "Clear destination");
+        }
+    }
+
+    @Subscribe
     public void onMenuOptionClicked(MenuOptionClicked e) {
         if (e.getMenuAction() == MenuAction.WALK && config.walk()) {
             e.consume();
+        }
+
+        if (e.getMenuOption().equals("Map walk here")) {
+            mapPoint = calculateMapPoint(/*client.isMenuOpen() ? lastMenuOpenedPoint :*/ client.getMouseCanvasPosition());
+            logger.debug("Walking to: {}", mapPoint.toString());
+            var mapWidget = Widgets.get(595, 38);
+
+            if (mapWidget != null) {
+                Widgets.get(595, 38).interact("Close");
+            }
+            overlay.setTile(mapPoint);
+//            executorService.execute(() -> Movement.walkTo(mapPoint));
+            mapWalking = true;
+        }
+
+        if (e.getMenuOption().equals("Clear Destination") && Movement.isWalking()) {
+            logger.debug("Stopping walking");
+            mapWalking = false;
         }
     }
 
@@ -86,5 +136,29 @@ public class WalkerPlugin extends Plugin {
             client.setUsername("");
             client.setPassword("");
         }
+    }
+
+    private WorldPoint calculateMapPoint(Point point) {
+        float zoom = client.getRenderOverview().getWorldMapZoom();
+        RenderOverview renderOverview = client.getRenderOverview();
+        final WorldPoint mapPoint = new WorldPoint(renderOverview.getWorldMapPosition().getX(), renderOverview.getWorldMapPosition().getY() + 40, 0);
+        final Point middle = worldMapOverlay.mapWorldPointToGraphicsPoint(mapPoint);
+
+        final int dx = (int) ((point.getX() - middle.getX()) / zoom);
+        final int dy = (int) ((-(point.getY() - middle.getY())) / zoom);
+
+        return mapPoint.dx(dx).dy(dy);
+    }
+
+    private void addMenuEntry(MenuEntryAdded event, String option) {
+        List<MenuEntry> entries = new LinkedList<>(Arrays.asList(client.getMenuEntries()));
+
+        MenuEntry entry = new MenuEntry();
+        entry.setOption(option);
+        entry.setTarget(event.getTarget());
+        entry.setOpcode(MenuAction.RUNELITE.getId());
+        entries.add(0, entry);
+
+        client.setMenuEntries(entries.toArray(new MenuEntry[0]));
     }
 }
