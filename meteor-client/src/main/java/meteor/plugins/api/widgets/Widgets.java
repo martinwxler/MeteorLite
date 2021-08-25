c09a5792e160876035ccf37ce9a97452facbebb0
binan <burakinan1@hotmail.com>
2021-08-25 23:02:03 +0200

    Add GrandExchange API

--- meteor-client/src/main/java/meteor/plugins/api/widgets/Widgets.java
+++ meteor-client/src/main/java/meteor/plugins/api/widgets/Widgets.java
@@ -6,6 +6,8 @@ import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
 
 public class Widgets {
 
@@ -15,5 +17,18 @@ public class Widgets {
 
     public static Widget get(int group, int id) {
         return Game.getClient().getWidget(group, id);
+    }
+
+    public static Widget get(int group, int id, int child) {
+        return get(group, id) == null ? null : get(group, id).getChild(child);
+    }
+
+    public static List<Widget> get(int group) {
+        Widget[] widgets = Game.getClient().getWidgets()[group];
+        if (widgets == null) {
+            return null;
+        }
+
+        return Arrays.asList(widgets);
     }
 }
