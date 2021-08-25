45ef9bd5e8ee91f740b427c7bbd476389e7d7132
c-madoc <60823564+c-madoc@users.noreply.github.com>
2021-08-24 19:16:48 -0600

    Prettify Method enums

--- meteor-client/src/main/java/meteor/plugins/autologhop/Method.java
+++ meteor-client/src/main/java/meteor/plugins/autologhop/Method.java
@@ -1,8 +1,20 @@
 package meteor.plugins.autologhop;
 
 public enum Method {
-    LOGOUT,
-    HOP,
-    LOGOUT_HOP,
-    TELEPORT
+    LOGOUT("Logout"),
+    HOP("Hop"),
+    LOGOUT_HOP("Logout, Hop"),
+    TELEPORT("Teleport");
+
+    private final String name;
+
+    Method(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
