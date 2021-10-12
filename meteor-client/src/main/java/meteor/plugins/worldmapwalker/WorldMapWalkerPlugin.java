22cd1a551796d2f96a599228019a1acbcdc2f7ae
SheinH <chocolateshein.htike@gmail.com>
2021-10-12 02:08:15 -0400

    WorldMapWalker - Added shift-click to walk + Walk path display.

--- meteor-client/src/main/java/meteor/plugins/worldmapwalker/WorldMapWalkerPlugin.java
+++ meteor-client/src/main/java/meteor/plugins/worldmapwalker/WorldMapWalkerPlugin.java
@@ -1,12 +1,19 @@
 package meteor.plugins.worldmapwalker;
 
+import com.google.inject.Provides;
+import meteor.config.ConfigManager;
 import meteor.eventbus.Subscribe;
+import meteor.input.KeyListener;
+import meteor.input.KeyManager;
 import meteor.menus.WidgetMenuOption;
 import meteor.plugins.Plugin;
 import meteor.plugins.PluginDescriptor;
 import meteor.plugins.api.entities.Players;
 import meteor.plugins.api.movement.Movement;
+import meteor.plugins.api.movement.Reachable;
+import meteor.plugins.api.movement.pathfinder.Walker;
 import meteor.plugins.api.widgets.Widgets;
+import meteor.ui.overlay.OverlayManager;
 import meteor.ui.overlay.worldmap.WorldMapOverlay;
 import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
@@ -16,9 +23,11 @@ import net.runelite.api.widgets.WidgetInfo;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static net.runelite.api.widgets.WidgetInfo.MINIMAP_WORLDMAP_OPTIONS;
 
@@ -32,21 +41,51 @@ public class WorldMapWalkerPlugin extends Plugin {
 
     @Inject
     private WorldMapOverlay worldMapOverlay;
+    @Inject
+    private KeyManager keyManager;
+    @Inject
+    WorldMapWalkerConfig config;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private WorldMapWalkerOverlay overlay;
+
+    private boolean hotKeyPressed;
 
     private Point lastMenuOpenedPoint;
-    private WorldPoint mapPoint;
+    WorldPoint mapPoint;
 
 
     private static final String DESTINATION_MENU_TARGET = "<col=00ff00>Destination";
 
     @Override
     public void startup() {
+        keyManager.registerKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                hotKeyPressed = e.isShiftDown();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                hotKeyPressed = e.isShiftDown();
+            }
+        },WorldMapWalkerPlugin.class);
+        overlayManager.add(overlay);
     }
 
     @Override
     public void shutdown() {
+        overlayManager.remove(overlay);
     }
 
+    @Provides
+    public WorldMapWalkerConfig getConfig(ConfigManager configManager) {
+        return configManager.getConfig(WorldMapWalkerConfig.class);
+    }
     @Subscribe
     public void onGameTick(GameTick event) {
         if (Movement.isWalking()) {
@@ -91,11 +130,16 @@ public class WorldMapWalkerPlugin extends Plugin {
         // If user clicks on map
         if (map != null && map.getBounds().contains(client.getMouseCanvasPosition().getX(), client.getMouseCanvasPosition().getY())) {
             if (!menuContainsEntries()) {
-                addMenuEntry(event, "Walk to",0);
-                addMenuEntry(event, "Clear",0);
-                logger.info("World map param1: " + event.getActionParam1());
+                if(hotKeyPressed){
+                    addMenuEntryFront(event, "Walk to");
+                    addMenuEntry(event, "Clear",0);
+                }
+                else{
+                    addMenuEntry(event, "Walk to",0);
+                    addMenuEntry(event, "Clear",0);
+                }
+                return;
             }
-            return;
         }
         // If user clicks on globe icon on minimap
         if (event.getActionParam1() == MINIMAP_WORLDMAP_OPTIONS.getId()) {
@@ -163,4 +207,25 @@ public class WorldMapWalkerPlugin extends Plugin {
 
         client.setMenuEntries(entries.toArray(new MenuEntry[0]));
     }
+    private void addMenuEntryFront(MenuEntryAdded event, String option) {
+        List<MenuEntry> entries = new LinkedList<>(Arrays.asList(client.getMenuEntries()));
+
+        MenuEntry entry = new MenuEntry();
+        entry.setOption(option);
+        entry.setTarget(DESTINATION_MENU_TARGET);
+        entry.setOpcode(MenuAction.RUNELITE.getId());
+        entries.add(entry);
+
+        client.setMenuEntries(entries.toArray(new MenuEntry[0]));
+    }
+
+    @Subscribe
+    private void onFocusChanged(FocusChanged focusChanged)
+    {
+        if (!focusChanged.isFocused())
+        {
+            hotKeyPressed = false;
+        }
+    }
+
 }
