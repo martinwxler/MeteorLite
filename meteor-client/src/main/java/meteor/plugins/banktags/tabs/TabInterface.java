84d0f021274ab3a4a98f86ab9653a3df33cf7f14
Burak <burakinan1@hotmail.com>
2021-10-14 10:50:39 +0200

    Merge w/ RuneLite/OPRS api

--- meteor-client/src/main/java/meteor/plugins/banktags/tabs/TabInterface.java
+++ meteor-client/src/main/java/meteor/plugins/banktags/tabs/TabInterface.java
@@ -249,6 +249,14 @@ public class TabInterface
 		equipmentButton.setOriginalY(4);
 		equipmentButton.revalidate();
 
+		Widget bankItemCountTop = client.getWidget(WidgetInfo.BANK_ITEM_COUNT_TOP);
+		if (bankItemCountTop == null)
+		{
+			return;
+		}
+
+		int equipmentButtonTotalWidth = equipmentButton.getWidth() + equipmentButton.getOriginalX() - bankItemCountTop.getOriginalX();
+
 		// the bank item count is 3 widgets
 		for (int child = WidgetInfo.BANK_ITEM_COUNT_TOP.getChildId(); child <= WidgetInfo.BANK_ITEM_COUNT_BOTTOM.getChildId(); child++)
 		{
@@ -258,13 +266,22 @@ public class TabInterface
 				return;
 			}
 
-			widget.setOriginalX(widget.getOriginalX() + equipmentButton.getWidth());
+			widget.setOriginalX(widget.getOriginalX() + equipmentButtonTotalWidth);
 			widget.revalidate();
 		}
 
 		titleBar.setOriginalX(equipmentButton.getWidth() / 2);
 		titleBar.setOriginalWidth(titleBar.getWidth() - equipmentButton.getWidth());
 		titleBar.revalidate();
+
+		Widget groupStorageButton = client.getWidget(WidgetInfo.BANK_GROUP_STORAGE_BUTTON);
+		if (groupStorageButton == null)
+		{
+			return;
+		}
+
+		groupStorageButton.setOriginalX(groupStorageButton.getOriginalX() + equipmentButtonTotalWidth);
+		groupStorageButton.revalidate();
 	}
 
 	private void handleDeposit(MenuOptionClicked event, Boolean inventory)
