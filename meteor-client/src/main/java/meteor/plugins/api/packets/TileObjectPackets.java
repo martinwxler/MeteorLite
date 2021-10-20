355e9d50cfe2a890917fe3938c52feb81974081a
SheinH <chocolateshein.htike@gmail.com>
2021-10-20 19:06:12 -0400

    Changed GameoBjectPackets to TileObjectPackets
    All methods are compatible with TileObjects

--- meteor-client/src/main/java/meteor/plugins/api/packets/TileObjectPackets.java
+++ meteor-client/src/main/java/meteor/plugins/api/packets/TileObjectPackets.java
@@ -1,22 +1,18 @@
 package meteor.plugins.api.packets;
 
 import meteor.plugins.api.game.Game;
-import net.runelite.api.GameObject;
-import net.runelite.api.NPC;
 import net.runelite.api.Point;
+import net.runelite.api.TileObject;
 import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.packets.PacketBufferNode;
 import net.runelite.api.packets.PacketWriter;
-import osrs.KeyHandler;
-import osrs.PlayerComposition;
-import osrs.WorldMapSectionType;
 
 import java.util.List;
 
-public class GameObjectPackets {
+public class TileObjectPackets {
 
-    public static void queueGameObjectActionPacket(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
+    public static void queueTileObjectActionPacket(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
         PacketWriter writer = Game.getClient().getPacketWriter();
         PacketBufferNode packet = Game.getClient().preparePacket(Game.getClient().getGameObjectActionPacket(), writer.getIsaacCipher());
         packet.getPacketBuffer().writeShort01A$api(objectID);
@@ -26,7 +22,7 @@ public class GameObjectPackets {
         writer.queuePacket(packet);
     }
 
-    public static void queueGameObjectAction2Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown){
+    public static void queueTileObjectAction2Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown){
         PacketWriter writer = Game.getClient().getPacketWriter();
         PacketBufferNode packet = Game.getClient().preparePacket(Game.getClient().getGameObjectAction2Packet(), writer.getIsaacCipher());
         packet.getPacketBuffer().writeShort01A$api(worldPointY);
@@ -36,7 +32,7 @@ public class GameObjectPackets {
         writer.queuePacket(packet);
     }
 
-    public static void queueGameObjectAction3Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
+    public static void queueTileObjectAction3Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
         PacketWriter writer = Game.getClient().getPacketWriter();
         PacketBufferNode packet = Game.getClient().preparePacket(Game.getClient().getGameObjectAction3Packet(), writer.getIsaacCipher());
         packet.getPacketBuffer().writeShort01$api(worldPointY);
@@ -45,7 +41,7 @@ public class GameObjectPackets {
         packet.getPacketBuffer().writeShortA$api(objectID);
         writer.queuePacket(packet);
     }
-    public static void queueGameObjectAction4Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
+    public static void queueTileObjectAction4Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
         PacketWriter writer = Game.getClient().getPacketWriter();
         PacketBufferNode packet = Game.getClient().preparePacket(Game.getClient().getGameObjectAction4Packet(), writer.getIsaacCipher());
         packet.getPacketBuffer().writeShortA$api(worldPointY);
@@ -54,7 +50,7 @@ public class GameObjectPackets {
         packet.getPacketBuffer().writeShort01A$api(objectID);
         writer.queuePacket(packet);
     }
-    public static void queueGameObjectAction5Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
+    public static void queueTileObjectAction5Packet(int objectID, int worldPointX, int worldPointY, int ctrlDown) {
         PacketWriter writer = Game.getClient().getPacketWriter();
         PacketBufferNode packet = Game.getClient().preparePacket(Game.getClient().getGameObjectAction5Packet(), writer.getIsaacCipher());
         packet.getPacketBuffer().writeShort01$api(objectID);
@@ -63,45 +59,45 @@ public class GameObjectPackets {
         packet.getPacketBuffer().writeByte01$api(ctrlDown);
         writer.queuePacket(packet);
     }
-    public static void gameObjectFirstOption(GameObject object,int ctrlDown){
+    public static void tileObjectFirstOption(TileObject object, int ctrlDown){
         Point p = object.menuPoint();
         LocalPoint lp = new LocalPoint(p.getX(),p.getY());
         WorldPoint wp = WorldPoint.fromScene(Game.getClient(),lp.getX(),lp.getY(),object.getPlane());
-        queueGameObjectActionPacket(object.getId(),wp.getX(),wp.getY(),ctrlDown);
+        queueTileObjectActionPacket(object.getId(),wp.getX(),wp.getY(),ctrlDown);
     }
-    public static void gameObjectSecondOption(GameObject object,int ctrlDown){
+    public static void tileObjectSecondOption(TileObject object,int ctrlDown){
         Point p = object.menuPoint();
         LocalPoint lp = new LocalPoint(p.getX(),p.getY());
         WorldPoint wp = WorldPoint.fromScene(Game.getClient(),lp.getX(),lp.getY(),object.getPlane());
-        queueGameObjectAction2Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
+        queueTileObjectAction2Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
     }
-    public static void gameObjectThirdOption(GameObject object,int ctrlDown){
+    public static void tileObjectThirdOption(TileObject object,int ctrlDown){
         Point p = object.menuPoint();
         LocalPoint lp = new LocalPoint(p.getX(),p.getY());
         WorldPoint wp = WorldPoint.fromScene(Game.getClient(),lp.getX(),lp.getY(),object.getPlane());
-        queueGameObjectAction3Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
+        queueTileObjectAction3Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
     }
-    public static void gameObjectFourthOption(GameObject object,int ctrlDown){
+    public static void tileObjectFourthOption(TileObject object,int ctrlDown){
         Point p = object.menuPoint();
         LocalPoint lp = new LocalPoint(p.getX(),p.getY());
         WorldPoint wp = WorldPoint.fromScene(Game.getClient(),lp.getX(),lp.getY(),object.getPlane());
-        queueGameObjectAction4Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
+        queueTileObjectAction4Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
     }
-    public static void gameObjectFifthOption(GameObject object,int ctrlDown){
+    public static void tileObjectFifthOption(TileObject object,int ctrlDown){
         Point p = object.menuPoint();
         LocalPoint lp = new LocalPoint(p.getX(),p.getY());
         WorldPoint wp = WorldPoint.fromScene(Game.getClient(),lp.getX(),lp.getY(),object.getPlane());
-        queueGameObjectAction5Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
+        queueTileObjectAction5Packet(object.getId(),wp.getX(),wp.getY(),ctrlDown);
     }
-    public static void gameObjectAction(GameObject object, String action, int ctrlDown) {
+    public static void tileObjectAction(TileObject object, String action, int ctrlDown) {
         List<String> actions = object.getActions();
         int index = actions.indexOf(action);
         switch (index) {
-            case 0 -> gameObjectFirstOption(object,ctrlDown);
-            case 1 -> gameObjectSecondOption(object,ctrlDown);
-            case 2 -> gameObjectThirdOption(object,ctrlDown);
-            case 3 -> gameObjectFourthOption(object,ctrlDown);
-            case 4 -> gameObjectFifthOption(object,ctrlDown);
+            case 0 -> tileObjectFirstOption(object,ctrlDown);
+            case 1 -> tileObjectSecondOption(object,ctrlDown);
+            case 2 -> tileObjectThirdOption(object,ctrlDown);
+            case 3 -> tileObjectFourthOption(object,ctrlDown);
+            case 4 -> tileObjectFifthOption(object,ctrlDown);
         }
     }
 }
