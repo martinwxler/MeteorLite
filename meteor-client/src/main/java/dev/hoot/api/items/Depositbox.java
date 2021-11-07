e812ae0089080070558202ab0b31b92f34216cb2
KJ <dargiekeegan@gmail.com>
2021-11-06 18:17:53 -0600

    Fixed Depositbox comments

--- meteor-client/src/main/java/dev/hoot/api/items/Depositbox.java
+++ meteor-client/src/main/java/dev/hoot/api/items/Depositbox.java
@@ -6,60 +6,39 @@ import dev.hoot.api.widgets.Widgets;
 import net.runelite.api.widgets.Widget;
 
 public class Depositbox {
-    public final Game game;
 
-    public Depositbox(Game game) {
-        this.game = game;
+    public static void depositInventory() {
+        Widget depositInventory = Widgets.get(192, 4);
+        if (depositInventory == null)
+            return;
+
+        depositInventory.interact(0);
     }
 
-    public void depositInventory() {
-        checkDepositboxOpen();
-        if (!Inventory.isEmpty()) {
-            Widget depositInventory = Widgets.get(192, 4);
-            if (depositInventory == null)
-                return;
-
-            depositInventory.interact(0);
-            Time.sleepUntil(Inventory::isEmpty, 5);
-        }
+    public static void depositEquipment() {
+        Widget depositEquipment = Widgets.get(192, 6);
+        if (depositEquipment == null)
+            return;
+        depositEquipment.interact(0);
+        Time.sleepUntil(() -> Equipment.getAll().size() == 0, 5);
     }
 
-    public void depositEquipment() {
-        checkDepositboxOpen();
-        if (Equipment.getAll().size() != 0) {
-            Widget depositEquipment = Widgets.get(192, 6);
-            if (depositEquipment == null)
-                return;
-            depositEquipment.interact(0);
-            Time.sleepUntil(() -> Equipment.getAll().size() == 0, 5);
-        }
-    }
-
-    public void depositLootingbag() {
-        checkDepositboxOpen();
+    public static void depositLootingbag() {
         Widget depositLootingbag = Widgets.get(192, 8);
         if (depositLootingbag == null)
             return;
         depositLootingbag.interact(0);
-
     }
 
-    private void checkDepositboxOpen() {
-        if (!isOpen()) {
-            throw new IllegalStateException("depositbox isn't open");
-        }
+    public static boolean isOpen() {
+        Widget depositBox = Widgets.get(192, 1);
+        return depositBox != null;
     }
 
-    public boolean isOpen() {
-        Widget open = Widgets.get(192, 1);
-        return open != null;
-    }
-
-    public void close() {
-        if (isOpen()) {
-            Widget exitDepositBox = Widgets.get(192, 1, 11);
-            exitDepositBox.interact(0);
-            Time.sleepUntil(() -> !isOpen(), 5);
-        }
+    public static void close() {
+        Widget exitDepositBox = Widgets.get(192, 1, 11);
+        if (exitDepositBox == null)
+            return;
+        exitDepositBox.interact(0);
     }
 }
